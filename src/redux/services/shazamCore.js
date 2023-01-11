375509ffc6d2d13b6f30b4a869b1a2d7d2a5c784
zydrith <zeeharith@gmail.com>
2023-01-11 17:26:03 +0800

    update3

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/', 
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', '046a763678msh6836b9f6dd6391ep1fa3bcjsn6a8894782fa1');
 
@@ -11,12 +11,20 @@ export const shazamCoreApi = createApi({
         }
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world'}),
-        getSongDetails: builder.query({ query: ({ songid}) => `/tracks/details?track_id=${songid}`}),
+        getTopCharts: builder.query({ query: () => 'v1/charts/world'}),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+        getSongDetails: builder.query({ query: ({ songid}) => `v1/tracks/details?track_id=${songid}`}),
+        getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` })
     }),
 });
 
 export const {
     useGetTopChartsQuery,
     useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsByGenreQuery,
 } = shazamCoreApi
